52582e98ad19d99bb2845434d0100559925e5609
taras <holyk.taras@gmail.com>
2019-05-07 22:37:54 +0300

    Added some headers

--- app/helpers/minfin-site.parser.js
+++ app/helpers/minfin-site.parser.js
@@ -2,12 +2,12 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const currencies = [
-  {code: 'usd', index: 0},
-  {code: 'eur', index: 1},
-  {code: 'rub', index: 2},
-  {code: 'pln', index: 3},
-  {code: 'gbp', index: 4},
-  {code: 'chf', index: 5}
+  { code: 'usd', index: 0 },
+  { code: 'eur', index: 1 },
+  { code: 'rub', index: 2 },
+  { code: 'pln', index: 3 },
+  { code: 'gbp', index: 4 },
+  { code: 'chf', index: 5 }
 ];
 
 async function getExchangeRates(params) {
@@ -38,7 +38,8 @@ function getUrlContent(url) {
     const options = {
       uri: url,
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36',
+        'Content-Type': 'text/html; charset=utf-8'
       },
       transform: function (body) {
         return cheerio.load(body);
@@ -60,7 +61,7 @@ function getCurrencyExchangeRates(exchangeRatesTable, currency) {
   exchangeRates.title = tableCell.find('a').text().trim();
 
   exchangeRates.cash = (function ([purchase, selling]) {
-    return {purchase, selling};
+    return { purchase, selling };
   })(getTableCellContent(tableCell, 1));
 
   exchangeRates.nbu = tableCell.eq(2)
@@ -70,7 +71,7 @@ function getCurrencyExchangeRates(exchangeRatesTable, currency) {
     }).text().trim();
 
   exchangeRates.black = (function ([purchase, selling]) {
-    return {purchase, selling};
+    return { purchase, selling };
   })(getTableCellContent(tableCell, 3));
 
   return exchangeRates;
@@ -79,8 +80,8 @@ function getCurrencyExchangeRates(exchangeRatesTable, currency) {
 function getTableCellContent(tableCell, index) {
   return tableCell.eq(index)
     .contents().filter((index, element) => {
-    return element.nodeType === 3;
-  }).text().trim().split(/\n+/);
+      return element.nodeType === 3;
+    }).text().trim().replace(/,/g, '.').split(/\n+/);
 }
 
-module.exports = {getExchangeRates};
+module.exports = { getExchangeRates };
